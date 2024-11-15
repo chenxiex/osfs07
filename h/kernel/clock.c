f99985c6f81c5dad91a5e73f5015650bd5ee5aa7
Anlor <chenxiex@outlook.com>
2024-11-15 20:56:37 +0800

    patch /h/

--- h/kernel/clock.c
+++ h/kernel/clock.c
@@ -41,7 +41,7 @@ PUBLIC void milli_delay(int milli_sec)
 {
         int t = get_ticks();
 
-        while(((get_ticks() - t) * 1000 / HZ) < milli_sec) {}
+        while(((get_ticks() - t) * 1000 / HZ) < milli_sec) {delay(1);}
 }
 
 /*======================================================================*
